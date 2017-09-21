ab28125a293f153693fd9cb4d6f839775efb0fa6
bobiscool <tomisugly@163.com>
2017-09-21 11:31:45 +0800

    [Keng] webpack 配置项需要entry写成array的形式 ！！！

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry:"./src/",
+  entry:["./src/index.js"],
   output:{
       path:path.resolve(__dirname,'dist'),
       filename:"wxdraw.js"
