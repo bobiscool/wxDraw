17575909d5698459f4e725db7d8f7bc54a37afe0
bobiscool <tomisugly@163.com>
2017-09-21 11:14:33 +0800

    [Chg] add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,15 @@ const path = require('path');
 module.exports = {
   entry:"./src/",
   output:{
-      path:path.resolve(__dirname,'dist')
-  }
+      path:path.resolve(__dirname,'dist'),
+      filename:"wxdraw.js"
+  },
+  module:{
+      rules:[
+          {
+              test:"/\.js$/",
+              include:path.resolve(__dirname,"src")
+          }
+      ]
+  }  
 }
