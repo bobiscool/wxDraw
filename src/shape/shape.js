b78446c1e1aa214800ee163e389c886d45c52d59
bobiscool <tomisugly@163.com>
2017-09-22 15:46:34 +0800

    [Chg]  Shape construction

--- src/shape/shape.js
+++ src/shape/shape.js
@@ -1,16 +1,25 @@
+/*
+ * @Author: Thunderball.Wu 
+ * @Date: 2017-09-22 15:45:51 
+ * @Last Modified by:   Thunderball.Wu 
+ * @Last Modified time: 2017-09-22 15:45:51 
+ * 在这里添加事件 
+ */
+
 
 var ShapeBase = require('./shapeBase.js').ShapeBase;
 var Circle = require('./normalShape.js').Circle;
 
-function Shape(type,option){
-   this.Shape = new Circle(option)
+function Shape(type,option,draggable){
+   this.draggable = draggable?true:false;
+   this.Shape = new shapeTypes[type](option);
 }
 
 
 
 
 
-var _shapeType={
+var shapeTypes={
    "circle": function(options){
        return new 
    }
