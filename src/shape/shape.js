7c26cc282d4d11997643b542a80449a083eb90ed
bobiscool <tomisugly@163.com>
2017-09-22 16:01:14 +0800

    [Chg]   fill or strike

--- src/shape/shape.js
+++ src/shape/shape.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Thunderball.Wu 
  * @Date: 2017-09-22 15:45:51 
- * @Last Modified by:   Thunderball.Wu 
- * @Last Modified time: 2017-09-22 15:45:51 
+ * @Last Modified by: Thunderball.Wu
+ * @Last Modified time: 2017-09-22 16:00:49
  * 在这里添加事件 
  */
 
@@ -10,18 +10,26 @@
 var ShapeBase = require('./shapeBase.js').ShapeBase;
 var Circle = require('./normalShape.js').Circle;
 
-function Shape(type,option,draggable){
+function Shape(type,option,strokeOrfill,draggable){
    this.draggable = draggable?true:false;
+   this.strokeOrfill = strokeOrfill?true:false;//是否填充
    this.Shape = new shapeTypes[type](option);
 }
 
 
 
+Shape.prototype = {
+    paint:function(){
+       if(strokeOrfill)
+    }
+}
+
+
 
 
 var shapeTypes={
-   "circle": function(options){
-       return new 
+   "circle": function(option){
+       return new Circle(option)
    }
 }
 
