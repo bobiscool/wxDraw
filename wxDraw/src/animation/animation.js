f00dbd0caf71b8e7771d4cc0bee01eff07d909aa
bobiscool <tomisugly@163.com>
2017-09-27 23:48:35 +0800

    [Chg] stopWatch

--- wxDraw/src/animation/animation.js
+++ wxDraw/src/animation/animation.js
@@ -2,10 +2,16 @@
  * @Author: Thunderball.Wu 
  * @Date: 2017-09-27 23:31:49 
  * @Last Modified by: Thunderball.Wu
- * @Last Modified time: 2017-09-27 23:43:47
+ * @Last Modified time: 2017-09-27 23:48:21
  * 单个小物件自己的计时器
  */
-
+function Watch(){
+    this.startTime = 0; //启动时间
+    this.running = false;//是否还在运行
+    this.goesBytime = 0;
+    this.goesBy = undefined;
+    this.DEFAULT_ELASTIC = 2;
+}
 export const AnimationTimer = function(duration,timeWrap){
     if(duration !== undefined) this.duration = duration;
     if(timeWrap !== undefined) this.timeWrap = timeWrap;
