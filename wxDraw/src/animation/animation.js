b652fe07f82feaed2c512669da2064da26392da9
bobiscool <tomisugly@163.com>
2017-09-27 23:44:09 +0800

    [Chg] animationTimer

--- wxDraw/src/animation/animation.js
+++ wxDraw/src/animation/animation.js
@@ -2,16 +2,17 @@
  * @Author: Thunderball.Wu 
  * @Date: 2017-09-27 23:31:49 
  * @Last Modified by: Thunderball.Wu
- * @Last Modified time: 2017-09-27 23:36:51
+ * @Last Modified time: 2017-09-27 23:43:47
  * 单个小物件自己的计时器
  */
 
-export const AnimationTimer = function(){
-
+export const AnimationTimer = function(duration,timeWrap){
+    if(duration !== undefined) this.duration = duration;
+    if(timeWrap !== undefined) this.timeWrap = timeWrap;
 }
 
 AnimationTimer.prototype = {
-
+  
 
     
 }
