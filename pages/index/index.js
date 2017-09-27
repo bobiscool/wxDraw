218996cc7d72d2e518337debb6b0d2d78432af46
bobiscool <tomisugly@163.com>
2017-09-27 10:11:43 +0800

    [Chg] gulp

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,8 @@
 //index.js
 //获取应用实例
 const app = getApp()
-var WxDraw = require("../../utils/wxDraw/index.js").WxDraw;
-var Shape = require("../../utils/wxDraw/index.js").Shape;
+var WxDraw = require("../../wxDraw/index.js").WxDraw;
+var Shape = require("../../wxDraw/index.js").Shape;
 Page({
   data: {
     userInfo: {},
