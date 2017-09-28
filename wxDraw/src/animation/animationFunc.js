2df1449b4ddd6a32a593b213e4495dd7a5ea53c6
bobiscool <tomisugly@163.com>
2017-09-28 14:04:14 +0800

    [Chg] time func

---
+++ wxDraw/src/animation/animationFunc.js
@@ -0,0 +1,38 @@
+/*
+ * @Author: Thunderball.Wu 
+ * @Date: 2017-09-28 13:43:40 
+ * @Last Modified by: Thunderball.Wu
+ * @Last Modified time: 2017-09-28 14:04:01
+  时间函数 基于
+  http://easings.net/zh-cn
+ */
+ 
+ 
+EasingFunctions = {
+  // 线性函数
+  linear: function (t) { return t },
+  // easeInQuad 函数
+  easeInQuad: function (t) { return t*t },
+  // easeOutQuad 函数
+  easeOutQuad: function (t) { return t*(2-t) },
+  // acceleration until halfway, then deceleration
+  easeInOutQuad: function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
+  // accelerating from zero velocity 
+  easeInCubic: function (t) { return t*t*t },
+  // decelerating to zero velocity 
+  easeOutCubic: function (t) { return (--t)*t*t+1 },
+  // acceleration until halfway, then deceleration 
+  easeInOutCubic: function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
+  // accelerating from zero velocity 
+  easeInQuart: function (t) { return t*t*t*t },
+  // decelerating to zero velocity 
+  easeOutQuart: function (t) { return 1-(--t)*t*t*t },
+  // acceleration until halfway, then deceleration
+  easeInOutQuart: function (t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
+  // accelerating from zero velocity
+  easeInQuint: function (t) { return t*t*t*t*t },
+  // decelerating to zero velocity
+  easeOutQuint: function (t) { return 1+(--t)*t*t*t*t },
+  // acceleration until halfway, then deceleration 
+  easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
+}
