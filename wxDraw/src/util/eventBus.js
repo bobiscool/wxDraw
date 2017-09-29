5fc50ce8dc4164e78b2c7255fe6b180c2943964d
bobiscool <tomisugly@163.com>
2017-09-29 15:53:29 +0800

    [Chg] eventBus

--- wxDraw/src/util/eventBus.js
+++ wxDraw/src/util/eventBus.js
@@ -2,7 +2,7 @@
  * @Author: Thunderball.Wu 
  * @Date: 2017-09-29 15:33:40 
  * @Last Modified by: Thunderball.Wu
- * @Last Modified time: 2017-09-29 15:48:32
+ * @Last Modified time: 2017-09-29 15:53:05
  * 事件对象
  * 
  */
@@ -16,7 +16,7 @@ eventBus.prototype = {
     add:function(name,event){
         //添加事件 初始化事件
         this.eventList.forEach(function(ele) {
-            if(ele.name==="name"){
+            if(ele.name===name){
                 ele.thingsList.push(event);
             }else{
                 this.eventList.push({
@@ -26,8 +26,15 @@ eventBus.prototype = {
             }
         }, this);
     },
-    dispatch:function(){
+    dispatch:function(name,scope,params){
        //执行事件
+       this.eventList.forEach(function(ele){
+           if(ele.name===name){
+               this.eventList.forEach(function(_ele){
+                   _ele.call(scope,params)
+               });
+           }
+       });
     },
     destroy:function(){
         // 取消事件
