c3c5ac7a56b7427fc755db66dd36e6bb07c691a3
bobiscool <tomisugly@163.com>
2017-10-09 18:47:52 +0800

    [Fix] evenbus bug

--- wxDraw/src/util/eventBus.js
+++ wxDraw/src/util/eventBus.js
@@ -2,7 +2,7 @@
  * @Author: Thunderball.Wu 
  * @Date: 2017-09-29 15:33:40 
  * @Last Modified by: Thunderball.Wu
- * @Last Modified time: 2017-10-09 18:44:47
+ * @Last Modified time: 2017-10-09 18:47:46
  * 事件对象
  * 
  */
@@ -15,31 +15,33 @@ export const eventBus = function () {
 eventBus.prototype = {
     add: function (name, scope, event) {
         //添加事件 初始化事件
-        console.log('添加'+name);
-        if (!this.eventList.length) {
+        console.log('添加' + name);
+        if (this.eventList.length) {
+            this.eventList.forEach(function (ele) {
+                if (ele.name == name) {
+                    ele.thingsList.push(event);//如果已经有了这个事件 那就 存list 并且退出程序
+                    return false;
+                }
+            }, this);
+           // 如果没有 那就再造一个
             this.eventList.push({
                 name: name,
                 scope: scope,
                 thingsList: [event]
-            })
-            return false;
-        }
+            });
 
-        this.eventList.forEach(function (ele) {
-            if (ele.name == name) {
-                ele.thingsList.push(event);
-                return false;
-            }
-        }, this);
-
-
-         this.eventList.push({
+        } else {
+            this.eventList.push({
                 name: name,
                 scope: scope,
                 thingsList: [event]
             });
-        
-        console.log(this.eventList);        
+        }
+
+
+
+
+        console.log(this.eventList);
     },
     dispatch: function (name, scope) {
         //执行事件 这里有两种状况  执行最外层或者是事件添加层 的scope 或者是 当地的scope
@@ -57,16 +59,16 @@ eventBus.prototype = {
 
         this.eventList.forEach(function (ele) {
             if (ele.name === name) {
-                console.log('触发'+name);
+                console.log('触发' + name);
                 ele.thingsList.forEach(function (_ele) {
                     if (scope !== "no") {
-                        _ele.call(scope,..._params)
+                        _ele.call(scope, ..._params)
                     } else {
-                        _ele.call(ele.scope,..._params);
+                        _ele.call(ele.scope, ..._params);
                     }
 
-                //  TODO 添加 解构 
-                    
+                    //  TODO 添加 解构 
+
 
                 });
             }
