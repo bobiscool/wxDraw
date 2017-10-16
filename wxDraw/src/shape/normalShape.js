f87c4d5c6b55f6c12a9478bdc9a7f7d3f93dda24
bobiscool <tomisugly@163.com>
2017-10-16 18:02:22 +0800

    [Chg] change drawing way of rect

--- wxDraw/src/shape/normalShape.js
+++ wxDraw/src/shape/normalShape.js
@@ -2,12 +2,13 @@
  * @Author: Thunderball.Wu 
  * @Date: 2017-09-22 14:23:52 
  * @Last Modified by: Thunderball.Wu
- * @Last Modified time: 2017-10-13 17:09:44
+ * @Last Modified time: 2017-10-16 18:01:08
  * 普通形状
  * 
  */
 import { util } from '../util/utils.js';
 import { Matrix } from '../util/matrix.js';
+import { Point } from "./mixins/points.js" //准备把rect 改成 点形式
 
 
 var cOption = {
@@ -50,6 +51,7 @@ export const Circle = function (option) {
     this._offsetX = 0;
     this._offsetY = 0;
     this.rotateOrigin = null;
+    
 }
 
 Circle.prototype = {
@@ -120,8 +122,8 @@ Circle.prototype = {
         this.Option = util.extend(option, this.Option);
         this.bus.dispatch('update', 'no');
     },
-    setRotateOrigin:function(loc){
-        this.rotateOrigin= loc;
+    setRotateOrigin: function (loc) {
+        this.rotateOrigin = loc;
     }
 }
 
@@ -140,7 +142,19 @@ export const Rect = function (option) {
     this._offsetX = 0;
     this._offsetY = 0;
     this.bus = null;
-    this.rotateOrigin = null;    
+    this.rotateOrigin = null;
+    this.oriPoints = [];
+    this._Points = [];
+
+    this.max = {
+        maxX: null,
+        maxY: null,
+        minX: null,
+        minY: null,
+    };
+    this.getOriPoints();
+    this.getPoints();
+    this.getMax();
 }
 
 Rect.prototype = {
@@ -166,18 +180,121 @@ Rect.prototype = {
         context.restore();
     },
     _draw: function (context) {
+        let changeMatrix = null;
+        let getchaMatrix = null;
+        let origin = null;
+        this.getPoints();//拿到所有真实点
+        // console.log('_POINTS',this._Points);
+        this.getMax();//所有真实点max min
+        this.createPath(context);//绘制
+    },
+    getOriPoints: function () {
+        let points = [];
+
+        points.push([this.Option.x - this.Option.w / 2, this.Option.y - this.Option.h / 2])
+        points.push([this.Option.x - this.Option.w / 2, this.Option.y + this.Option.h / 2])
+        points.push([this.Option.x + this.Option.w / 2, this.Option.y - this.Option.h / 2])
+        points.push([this.Option.x + this.Option.w / 2, this.Option.y + this.Option.h / 2])
+
+        this.oriPoints = points;
+    },
+    getPoints: function () {
+        let _points = [];
+        let origin = null;
         if (!this.rotateOrigin) {
-            context.translate(this.Option.x + this.Option.w / 2, this.Option.y + this.Option.h / 2);// 坐标原点变为 矩形 中心
-            context.rotate(this.Option.rotate);
-            context.rect(-this.Option.w / 2, -this.Option.h / 2, this.Option.w, this.Option.h);
+            origin = [this.Option.x, this.Option.y];
         } else {
-            /**
-             * 这里需要注意  在设置 旋转中心后  旋转的 位置点将变为rect 左上角
-             */
-            context.translate(this.rotateOrigin[0], this.rotateOrigin[1]);
-            context.rotate(this.Option.rotate);
-            context.rect(this.Option.x - this.rotateOrigin[0], this.Option.y - this.rotateOrigin[1], this.Option.w, this.Option.h);
+            origin = this.rotateOrigin;
         }
+
+        // console.log('item', origin);
+
+        this.oriPoints.forEach(function (item) {
+            _points.push(this.getPointTodraw(item[0], item[1], origin))
+        }, this);
+
+        this._Points = matrixToarray(_points);//除掉矩阵多余的部分
+        // console.log(this._Points);
+        // console.log(this.oriPoints);
+        return this._Points;//除掉矩阵多余的部分;
+    },
+    getPointTodraw: function (x, y, origin) {
+        let angle = this.Option.rotate;
+        //将所有变化 都转到 Point对象去了 
+        return new Point(x, y).rotate(origin, angle);//计算出每一个点变化之后的位置
+    },
+    getMax: function () {
+        //绘制 与检测 不能在统一个地方
+        let _Points = this._Points;
+
+        this.max = {
+            maxX: null,
+            maxY: null,
+            minX: null,
+            minY: null,
+        };
+
+        _Points.forEach(function (element) {
+            if (element[0] > this.max.maxX) {
+                this.max.maxX = element[0];
+            }
+            if (!this.max.minX && this.max.minX !== 0) {
+                this.max.minX = element[0];
+            }
+            if (this.max.minX && element[0] < this.max.minX) {
+                this.max.minX = element[0];
+            }
+
+
+
+            if (element[1] > this.max.maxY) {
+                this.max.maxY = element[1];
+            }
+            if (!this.max.minY && this.max.minY !== 0) {
+                this.max.minY = element[1];
+            }
+            if (this.max.minY && element[1] < this.max.minY) {
+                this.max.minY = element[1];
+            }
+        }, this);
+
+
+    },
+    createPath: function (context) {
+        //创建路径
+        var points = this._Points;
+
+        context.beginPath();
+        context.moveTo(points[0][0], points[0][1]);
+        for (var i = 1; i < this.Option.sides; ++i) {
+            context.lineTo(points[i][0], points[i][1]);
+        }
+        context.closePath();
+    },
+    _pnpolyTest(x, y) {
+        // 核心测试代码 理论源于  https://wrf.ecse.rpi.edu//Research/Short_Notes/pnpoly.html
+        // var A = this.points[0];// 拿到前面两个点
+        // var B = this.points[1];
+        var ifInside = false;
+
+        for (var i = 0, j = this._Points.length - 1; i < this._Points.length; j = i++) {
+            /**
+             * 0 4
+               1 0
+               2 1
+               3 2
+               4 3
+             */
+            var Xi = this._Points[i][0], Yi = this._Points[i][1];
+            var Xj = this._Points[j][0], Yj = this._Points[j][1];
+
+            var insect = ((Yi > y) != (Yj > y)) && (x < (Xj - Xi) * (y - Yi) / (Yj - Yi) + Xi);
+
+            if (insect) ifInside = !ifInside;
+        }
+
+        // console.log(ifInside);
+        return ifInside;
     },
     move: function (x, y) {
         this.Option.x = x;
@@ -189,18 +306,26 @@ Rect.prototype = {
         var _self = this;
 
         // console.log('方块', _self.Option.x, x, _self.Option.y, y, (_self.Option.y + _self.Option.h), y, (_self.Option.x + _self.Option.w), x);
-        if (_self.Option.x < x && _self.Option.y < y && (_self.Option.y + _self.Option.h) > y && (_self.Option.x + _self.Option.w) > x) {
-            this._offsetX = x - _self.Option.x;
-            this._offsetY = y - _self.Option.y;
-            console.log('移动方块');
-            this._isChoosed = true;
-            return true;// 点击
+         if (x > this.max.minX && x < this.max.maxX && y > this.max.minY && y < this.max.maxY) {
+            //在最小矩形里面才开始
+            // console.log('点中');
+            // this.points = this._Points;
+
+            this._offsetX = this.Option.x - x;
+            this._offsetY = this.Option.y - y;
+            if (this._pnpolyTest(x, y)) {
+                this._isChoosed = true;
+                return true;
+            }
         }
     },
     moveDetect: function (x, y) {
 
         if (this._isChoosed == true) {
             this.move(x - this._offsetX, y - this._offsetY);
+            this.getOriPoints();//拿到原始点
+            this.getPoints();//拿到变化点
+            this.getMax();//拿到边界点
         }
 
     },
@@ -212,8 +337,8 @@ Rect.prototype = {
         this.Option = util.extend(option, this.Option);
         this.bus.dispatch('update', 'no');
     },
-     setRotateOrigin:function(loc){
-        this.rotateOrigin= loc;
+    setRotateOrigin: function (loc) {
+        this.rotateOrigin = loc;
     }
 
 }
