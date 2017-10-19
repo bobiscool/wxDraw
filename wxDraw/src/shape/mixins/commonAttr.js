6ab741a9752fd0dd37eb47ce901546d174243af9
bobiscool <tomisugly@163.com>
2017-10-19 17:59:03 +0800

    [Chg]  more attrbutes

--- wxDraw/src/shape/mixins/commonAttr.js
+++ wxDraw/src/shape/mixins/commonAttr.js
@@ -2,12 +2,13 @@
  * @Author: Thunderball.Wu 
  * @Date: 2017-10-19 16:52:13 
  * @Last Modified by: Thunderball.Wu
- * @Last Modified time: 2017-10-19 16:56:25
+ * @Last Modified time: 2017-10-19 17:58:37
  * 常用的一些属性
  * 
  */
 
  export const  commonAttr = {
+     //这些样式是可以被动画来设置的
       lineWidth:12,//线宽
       Shadow:{
           offsetX:5,
@@ -17,3 +18,11 @@
       }
 
  }
+
+
+
+export const commonUnAttr = {
+    lineCap:"",      // lineCap	String	'butt'、'round'、'square'	线条的结束端点样式
+    lineJoin:"",   //lineJoin	String	'bevel'、'round'、'miter'	线条的结束交点样式
+    miterLimit:""   //最大斜接长度
+} 
