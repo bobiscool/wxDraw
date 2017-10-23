02b7766c16a661acedb3cf425b3b6d2ac2f944b0
bobiscool <tomisugly@163.com>
2017-10-23 11:07:52 +0800

    [Chg] add text

--- wxDraw/src/shape/text.js
+++ wxDraw/src/shape/text.js
@@ -2,7 +2,7 @@
  * @Author: Thunderball.Wu 
  * @Date: 2017-10-23 10:27:35 
  * @Last Modified by: Thunderball.Wu
- * @Last Modified time: 2017-10-23 10:56:10
+ * @Last Modified time: 2017-10-23 11:07:47
  * 字体对象
  */
 
@@ -10,28 +10,33 @@ import { util, matrixToarray } from '../util/utils.js';
 import { commonAttr, commonUnAttr } from "./mixins/commonAttr.js"; //共有属性
 import { commonMethods } from "./mixins/commonMethods.js"; //共有方法
 
-let alignBaseline =function(Option,type,w,h) {
+let baseline = function (type, h) {
     return {
-        "normal":-2,
-        "bottom":0,
-        "middle":-h/2,
-        "top":-h,
-        "left":0,
-        "center":-w/2,
-        "right":-w
-    }
+        "normal": 2,
+        "bottom": -h / 2,
+        "middle": 0,
+        "top": h / 2,
+
+    }[type]
 }
 
+let align = function (type, w) {
+    return {
+        "left": w / 2,
+        "center": 0,
+        "right": -w / 2
+    }[type]
+}
 
 export const Text = function (option) {
-    
-    if(!option.text){
+
+    if (!option.text) {
         return false;
     }
     var tOption = {
-        x:100,
-        y:200,
-        fontSize:12,
+        x: 100,
+        y: 200,
+        fontSize: 12,
         Shadow: {
             offsetX: 5,
             offsetY: 5,
@@ -43,27 +48,34 @@ export const Text = function (option) {
         rotate: 0,
         opacity: 1
     }
-   
+
     var tUnoption = {
-        textBaseline:"normal",
-        align:"left"
+        textBaseline: "normal",
+        align: "left"
     }
 
     this.text = option.text;
-    this.Option = util.extend(option,tOption);
-    this.Unoption = util.extend(option,tUnoption);
-} 
+    this.Option = util.extend(option, tOption);
+    this.Unoption = util.extend(option, tUnoption);
+    this.boxOption = { x: 0, y: 0 };
+    this.boxOriPoints=[];
+}
 
 
 Text.prototype = {
-    getOriPoints(){
+    getOriPoints() {
         //根据 字体 估算出器背后box大小 位置
         // 这里还要根据 baseline textalgin来计算 box位置
         let points = [];
         let len = String(this.text).length;
-        let w = len*this.Option.fontSize;
-        let h = len*this.Option.fontSize;
+        let w = len * this.Option.fontSize;
+        let h = len * this.Option.fontSize;
 
+        this.boxOption.x =this.Option.x+align(this.Unoption.align,w);
+        this.boxOption.y =this.Option.y+baseline(this.Unoption.baseline,h);
         
-    } 
+        this.boxOriPoints = [];
+        this.boxOriPoints = [];
+        
+    }
 }
